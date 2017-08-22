06d6bc9591f83bfe2a313463dc2a898d779ad446
Eugene Ozhinsky <eugene.ozhinsky@ucsf.edu>
2017-08-22 16:26:28 -0700

    Initial Commit

---
+++ PictureScreenSaver/PictureScreenSaverView.h
@@ -0,0 +1,13 @@
+//
+//  PictureScreenSaverView.h
+//  PictureScreenSaver
+//
+//  Created by Eugene Ozhinsky on 8/22/17.
+//  Copyright © 2017 Eugene Ozhinsky. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface PictureScreenSaverView : ScreenSaverView
+
+@end
