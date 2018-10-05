fa737f437747bcb7123bb82de722c7fb79531492
Eugene Ozhinsky <eugene.ozhinsky@ucsf.edu>
2018-10-05 16:48:01 -0700

    implemented saving and loading directory list

--- PictureScreenSaver/ConfWindowController.swift
+++ PictureScreenSaver/ConfWindowController.swift
@@ -13,13 +13,12 @@ class ConfWindowController: NSWindowController, NSTextFieldDelegate {
 
     let defaults = ScreenSaverDefaults(forModuleWithName: "eugene-o.PictureScreenSaver")
 
-    var data: [String] = []
+    var directories: [String] = []
     
     @IBOutlet weak var intervalStepper: NSStepper!
     @IBOutlet weak var transitionStepper: NSStepper!
     @IBOutlet weak var transitionField: NSTextField!
     @IBOutlet weak var okButton: NSButton!
-//    @IBOutlet weak var fileNameField: NSTextField!
     @IBOutlet weak var intervalField: NSTextField!
     
     @IBOutlet weak var folderTableView: NSTableView!
@@ -37,7 +36,7 @@ class ConfWindowController: NSWindowController, NSTextFieldDelegate {
             return
         }
         
-//        defaults?.set(fileNameField.stringValue, forKey: "directory")
+        defaults?.set(directories, forKey: "directories")
         defaults?.set(intervalField.intValue, forKey: "interval")
         defaults?.set(transitionField.intValue, forKey: "transition")
         defaults?.synchronize()
@@ -48,34 +47,6 @@ class ConfWindowController: NSWindowController, NSTextFieldDelegate {
         }
         NSApp.mainWindow?.endSheet(window!)
     }
-/*
-    @IBAction func browseButtonPressed(_ sender: Any) {
-        let dialog = NSOpenPanel();
-        
-        dialog.title                   = "Choose a folder with images";
-        dialog.showsResizeIndicator    = true;
-        dialog.showsHiddenFiles        = false;
-        dialog.canChooseFiles          = false;
-        dialog.canChooseDirectories    = true;
-        dialog.canCreateDirectories    = false;
-        dialog.allowsMultipleSelection = false;
-        //dialog.allowedFileTypes        = ["txt"];
-        
-        if (dialog.runModal() == NSModalResponseOK) {
-            let result = dialog.url // Pathname of the file
-            
-            if (result != nil) {
-                let path = result!.path
-                fileNameField.stringValue = path
-            }
-        } else {
-            // User clicked on "Cancel"
-            return
-        }
-        
-    }
-
-*/
     
     @IBAction func plusButtonPressed(_ sender: Any) {
         NSLog("EO plusButtonPressed");
@@ -97,8 +68,8 @@ class ConfWindowController: NSWindowController, NSTextFieldDelegate {
             if (result != nil) {
                 let path = result!.path
                 NSLog("EO appending path...");
-                data.append(path)
-                NSLog("EO %@", data.description);
+                directories.append(path)
+                NSLog("EO %@", directories.description);
                 folderTableView.reloadData()
             }
         } else {
@@ -111,7 +82,7 @@ class ConfWindowController: NSWindowController, NSTextFieldDelegate {
     @IBAction func minusButtonPressed(_ sender: Any) {
         NSLog("EO minusButtonPressed");
         if(folderTableView.selectedRow >= 0 ){
-            data.remove(at: folderTableView.selectedRow)
+            directories.remove(at: folderTableView.selectedRow)
             folderTableView.reloadData()
         }
     }
@@ -144,8 +115,8 @@ class ConfWindowController: NSWindowController, NSTextFieldDelegate {
     }
     
     func loadDefaults(){
-        if let directory = defaults?.string(forKey: "directory"){
-//            fileNameField.stringValue = directory
+        if let directories = defaults?.stringArray(forKey: "directories"){
+            self.directories = directories
         }
         if let interval = defaults?.integer(forKey: "interval"){
             if interval > 0 {
@@ -170,13 +141,13 @@ extension ConfWindowController: NSTableViewDataSource, NSTableViewDelegate {
     
     func numberOfRows(in tableView: NSTableView) -> Int {
         NSLog("EO numberOfRows");
-        return data.count
+        return directories.count
     }
  
     func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
 //    func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
         NSLog("EO tableView");
-        let item = (data)[row]
+        let item = (directories)[row]
         
         let cell = tableView.make(withIdentifier: (tableColumn!.identifier), owner: self) as? NSTableCellView
         cell?.textField?.stringValue = item
